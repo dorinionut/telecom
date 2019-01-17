04b30174e18386a25d8a6207b8f2ddb34b33cf28
David <dorin.david@cgm.com>
2019-01-17 16:36:55 +0100

    Update to Angular7

--- src/app/service/cart.service.ts
+++ src/app/service/cart.service.ts
@@ -1,13 +1,9 @@
 import { Injectable } from '@angular/core';
-import { Subject } from 'rxjs/Subject';
 
 import { Carrier } from '../model/carrier.model';
-import { CarrierService } from '../service/carrier.service';
 import { Option } from '../model/option.model';
-import { OptionService } from '../service/option.service';
 import { Person } from '../model/person.model';
 import { Plan } from '../model/plan.model';
-import { PlanService } from '../service/plan.service';
 
 @Injectable()
 export class CartService {
